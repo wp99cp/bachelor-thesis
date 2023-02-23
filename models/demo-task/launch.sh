609c00369c1b9de820976afcf3aac8a37ee8f758
Cyrill Püntener <wp99cp@gmx.net>
2023-02-23 10:25:54 +0100

    update launch script

--- models/demo-task/launch.sh
+++ models/demo-task/launch.sh
@@ -3,9 +3,10 @@
 #SBATCH -n 1
 #SBATCH --time=00:10:00
 #SBATCH --mem-per-cpu=512
-#SBATCH --output=slurm_{job_id}_output.txt
-#SBATCH --error=slurm_{job_id}_error.txt
 #SBATCH --open-mode=truncate
+#SBATCH --tmp=3000
+#SBATCH --gpus=1
+#SBATCH --mail-type=END
 
 module load python
 module load cuda/11.7.0
