fix min calculation of num_workers

models/unet/main.py
@@ -45,7 +45,7 @@ def load_data():
                                  transforms=transforms, apply_augmentations=False)
 
     # create the training and test data loaders
-    num_workers = np.min(os.cpu_count(), 16)
+    num_workers = min(os.cpu_count(), 16)
     trainLoader = DataLoader(trainDS, shuffle=True, batch_size=BATCH_SIZE,
                              pin_memory=PIN_MEMORY, num_workers=num_workers)
     testLoader = DataLoader(testDS, shuffle=False, batch_size=BATCH_SIZE,