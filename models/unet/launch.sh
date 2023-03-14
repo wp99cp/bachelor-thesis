4e36a6355132dd9e69bfc9833d0633315e96f975
Cyrill Püntener <wp99cp@gmx.net>
2023-03-14 14:02:28 +0100

    update launch script

--- models/unet/launch.sh
+++ models/unet/launch.sh
@@ -4,14 +4,23 @@
 #SBATCH --time=00:10:00
 #SBATCH --mem-per-cpu=512
 #SBATCH --open-mode=truncate
+#SBATCH --output=/cluster/scratch/pucyril/%j/log/output.out
+#SBATCH --error=/cluster/scratch/pucyril/%j/log/error.out
 #SBATCH --tmp=3000
 #SBATCH --gpus=1
 #SBATCH --mail-type=END
 
+# load modules
 module load python
 module load cuda/11.7.0
+module list
 
-# Configs
+# install dependencies
+pip install --user imutils==0.5.4
+pip install --user pytorch-model-summary==0.1.2
+
+# Dataset Config
 export DATASET="/cluster/scratch/pucyril/dataset.zip"
 
+# start the job
 source job.sh
