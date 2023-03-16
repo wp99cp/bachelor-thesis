71e6fdfb08fd538d294f64dc8f7da4e786560e24
Cyrill Püntener <wp99cp@gmx.net>
2023-03-16 20:20:26 +0100

    add retrain as flag not as argument

--- models/unet/task.sh
+++ models/unet/task.sh
@@ -2,17 +2,17 @@ echo "All further logs are saved in $LOG_DIR/python.log"
 
 # Define a signal handler function to forward USR1 signals to the Python process
 handle_signal() {
-    if [ $1 = "USR1" ]; then
-        echo "Forwarding USR1 signal to Python process"
-        kill -USR1 "$PYTHON_PID"
-    fi
+  if [ "$1" = "USR1" ]; then
+    echo "Forwarding USR1 signal to Python process"
+    kill -USR1 "$PYTHON_PID"
+  fi
 }
 
 # Set the signal handler function for USR1 signals
 trap 'handle_signal USR1' USR1
 
 # Start the Python process in the background and save its PID
-python main.py retrain 3>&1 1>>$LOG_DIR/python.log 2>&1 &
+python main.py --retrain 3>&1 1>>"$LOG_DIR/python.log" 2>&1 &
 PYTHON_PID=$!
 
 # Wait for the Python process to finish
