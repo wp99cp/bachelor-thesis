5517418fa826951fa220ca8bb3a4e83086c025ae
Cyrill Püntener <wp99cp@gmx.net>
2023-03-16 20:43:57 +0100

    try to fix signal forwarding

--- models/unet/task.sh
+++ models/unet/task.sh
@@ -4,6 +4,7 @@ echo "All further logs are saved in $LOG_DIR/python.log"
 handle_signal() {
   if [ "$1" = "USR1" ]; then
     echo "Forwarding USR1 signal to Python process"
+    echo "this is done using the following command: kill -USR1 $PYTHON_PID"
     kill -USR1 "$PYTHON_PID"
   fi
 }
@@ -12,7 +13,7 @@ handle_signal() {
 trap 'handle_signal USR1' USR1
 
 # Start the Python process in the background and save its PID
-python main.py --retrain 3>&1 1>>"$LOG_DIR/python.log" 2>&1 &
+python main.py --retrain >"$LOG_DIR/python.log" 2>&1 &
 PYTHON_PID=$!
 
 # Wait for the Python process to finish
