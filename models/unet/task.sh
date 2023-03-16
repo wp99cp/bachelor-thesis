fe6ac54c54109b05ee00ccf38fcd801956a9325f
Cyrill Püntener <wp99cp@gmx.net>
2023-03-16 19:36:42 +0100

    fix launch to fully run python script

--- models/unet/task.sh
+++ models/unet/task.sh
@@ -2,10 +2,12 @@ echo "All further logs are saved in $LOG_DIR/python.log"
 
 # run the actual task
 python main.py --retrain >>"$LOG_DIR/python.log" &
+pid_py=$!
 
 # trap om USR1, SIGTERM, SIGKILL, and SIGINT and echo the signal id
 # this is used to save the model before the slurm job is killed
-trap 'echo "Received USR1 signal, forwarding to python process (PID=$!)" && kill -SIGUSR1 $!' USR1
+trap 'echo "Received USR1 signal, forwarding to python process (PID=$pid_py)" && kill -SIGUSR1 $pid_py' USR1
 
 # wait for the python process to finish
-wait
+wait $pid_py
+echo "Python process finished, cleaning up"
