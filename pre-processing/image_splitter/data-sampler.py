6697729df66f57aeaf6fdfab752304f43273e254
Cyrill Püntener <wp99cp@gmx.net>
2023-03-28 17:26:16 +0200

    show total number of dates

--- pre-processing/image_splitter/data-sampler.py
+++ pre-processing/image_splitter/data-sampler.py
@@ -209,10 +209,10 @@ def main():
     pixel_count = [0] * NUM_ENCODED_CHANNELS
 
     # Iterate over all dates
-    for date in dates:
+    for i, date in enumerate(dates):
 
         date_str = date[0:4] + '-' + date[4:6] + '-' + date[6:8]
-        print(f"\nStart processing Date: {date_str}")
+        print(f"\nStart processing Date: {date_str}. That's {i + 1} of {len(dates)}.")
 
         bands = open_date(date)
         bands = bands / 10_000 * 255
