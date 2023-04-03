fd08112a92aa6c27a443dfcf888eb26042eae40b
Cyrill Püntener <pucyril@ethz.ch>
2023-04-03 20:18:59 +0200

    rebalance class, adjust class weigths

--- models/unet/configs/config.py
+++ models/unet/configs/config.py
@@ -15,7 +15,7 @@ NUM_CHANNELS = 13  # all satellite images have 13 channels
 NUM_CLASSES = 4
 CLASS_NAMES = ["background", "snow", "clouds", "water"]  # "thin_clouds"
 NUM_ENCODED_CHANNELS = 5  # Number of channels used to encode the grayscale image
-CLASS_WEIGHTS = [0.41002, 0.29181, 0.29019, 0.00798]  # class weights for background, snow, clouds, water
+CLASS_WEIGHTS = [0.51181, 0.33982, 0.13864, 0.00973]  # class weights for background, snow, clouds, water
 
 # define threshold to filter weak predictions
 THRESHOLD = 0.65
